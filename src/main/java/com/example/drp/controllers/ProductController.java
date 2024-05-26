d59aea81d69fb0e98470fd083f84789539e25fac
Joao Junior <joaojunior1205@gmail.com>
2024-05-26 20:20:22 -0300

    feat: query product related

--- src/main/java/com/example/drp/controllers/ProductController.java
+++ src/main/java/com/example/drp/controllers/ProductController.java
@@ -1,8 +1,11 @@
 package com.example.drp.controllers;
 
+import com.example.drp.domain.customizedFields.CustomizedField;
+import com.example.drp.domain.customizedFields.CustomizedFieldResponseDTO;
 import com.example.drp.domain.product.*;
 import com.example.drp.domain.user.User;
 import com.example.drp.infra.security.TokenService;
+import com.example.drp.repositories.CustomizedFieldRepository;
 import com.example.drp.repositories.ProductRepository;
 import com.example.drp.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +20,9 @@ public class ProductController {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private CustomizedFieldRepository customizedFieldRepository;
 
     @Autowired
     private UserRepository userRepository;
@@ -43,7 +49,17 @@ public class ProductController {
 
     @GetMapping
     public List<ProductResponseDTO> getAll() {
-        List<ProductResponseDTO> productList = productRepository.findAll().stream().map(ProductResponseDTO::new).toList();
+        List<ProductResponseDTO> productList = productRepository.findAll().stream().map(product -> {
+            List<Object[]> getCustomfields = customizedFieldRepository.findCustomizedFieldsByProductId(product.getId());
+            List<CustomizedFieldResponseDTO> customFields = new java.util.ArrayList<>();
+
+            for (Object[] getCustomfield : getCustomfields) {
+                customFields.add(new CustomizedFieldResponseDTO((CustomizedField) getCustomfield[0], (String) getCustomfield[1]));
+            }
+
+            return new ProductResponseDTO(product, customFields);
+        }).toList();
+
 
         return productList;
     }
--- src/main/java/com/example/drp/repositories/CustomizedFieldRepository.java
+++ src/main/java/com/example/drp/repositories/CustomizedFieldRepository.java
@@ -1,7 +1,6 @@
 package com.example.drp.repositories;
 
 import com.example.drp.domain.customizedFields.CustomizedField;
-import com.example.drp.domain.customizedFields.CustomizedFieldResponseDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -9,5 +8,5 @@ import java.util.List;
 
 public interface CustomizedFieldRepository extends JpaRepository<CustomizedField, Long> {
     @Query("SELECT cf, pcf.value FROM customizedField cf JOIN ProductCustomizedField pcf ON cf.id = pcf.customizedFieldId WHERE pcf.productId = :productId")
-    List<CustomizedFieldResponseDTO> findCustomizedFieldsByProductId(Long productId);
+    List<Object[]> findCustomizedFieldsByProductId(Long productId);
 }
